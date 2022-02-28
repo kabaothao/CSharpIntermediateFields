f6cd7695052d9ab3327537139e970b75532361e4
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-28 11:10:15 -0600

    updated codes

---
+++ CSharpIntermediateFields/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediateFields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Order;  
+
+        public Customer()
+        {
+            Order = new List<Order>(); //You should always initialize that list to an empty list.
+        }
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+    }
+}
+
+
+/*
+ 
+ Now if you remember from the lecture about constructors I explained why we need to initialize these
+
+orders to an empty list.
+
+Just to recap whenever you have a class like customer and inside the class you have a list.
+
+You should always initialize that list to an empty list.
+
+
+
+ 
+ */
--- CSharpIntermediateFields/Program.cs
+++ CSharpIntermediateFields/Program.cs
@@ -5,6 +5,8 @@ using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -67,5 +69,34 @@ This approach has an extra benefit and that is we can have multiple constructors
 
 one is called the orders field will always be initialized to an empty list.
 
+
+
+* Read -only Fields
+We can declare a field with the read only modifier to make sure that that field is only assigned to
+
+once.
+
+So in that case that field has to be initialized either directly like this.
+
+Like you see here or in one of the constructors of the class.
+
+
+    public class Customer 
+    {
+        readonly List<Order> Orders = new List<Order>();
+    }
+
+
+
+
+The reason is to create some kind of safety in our application to improve robustness.
+
+For example take a look at this customer class here by declaring the orders field as read only.
+
+I'll make sure that I will only have one list where we will keep the list of orders.
+
+If I accidentally as a developer try to re-initialize this field in another method the data that we
+
+had in the field will not be lost.
 
  */
