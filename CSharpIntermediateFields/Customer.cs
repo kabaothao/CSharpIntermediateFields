6d6acf375559c4e7e7a94df29b25ae8bcc7f1332
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-28 11:19:10 -0600

    udpated class

--- CSharpIntermediateFields/Customer.cs
+++ CSharpIntermediateFields/Customer.cs
@@ -10,12 +10,9 @@ namespace CSharpIntermediateFields
     {
         public int Id;
         public string Name;
-        public List<Order> Order;  
+        public List<Order> Orders = new List<Order>();  //This way no matter which constructor is going to be called the Order spell will always be initialized an empty list.
 
-        public Customer()
-        {
-            Order = new List<Order>(); //You should always initialize that list to an empty list.
-        }
+
         public Customer(int id)
         {
             this.Id = id;
@@ -27,6 +24,14 @@ namespace CSharpIntermediateFields
         {
             this.Name = name;
         }
+
+
+        //read only file
+        public void Promote()
+        {
+
+        }
+
 
     }
 }
@@ -42,7 +47,21 @@ Just to recap whenever you have a class like customer and inside the class you h
 
 You should always initialize that list to an empty list.
 
+        public Customer()
+        {
+            Order = new List<Order>(); //You should always initialize that list to an empty list.
+                                       //We have to make sure that the default constructor is always called first. So the orders fill will be initialized.
 
+        }
+
+
+The approach you choose is up to you.
+
+But what I want to emphasize to you is to be consistent with your approach.
+
+Make sure you use the same approach everywhere.
+
+This way your code would be cleaner.
 
  
  */
